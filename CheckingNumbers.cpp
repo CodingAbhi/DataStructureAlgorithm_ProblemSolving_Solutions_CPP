42ab6f8d8ecdf3289b08260b93455b4ee89d27de
Abhishek <abhishek8112003@gmail.com>
2022-10-09 01:17:26 +0530

    feat: CheckNumber,oddeven

---
+++ CheckingNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+
+    cin >> a >>b;
+if (a==b) {
+    cout<<" Both are equal";
+    } else {
+        if (a>b) {
+        cout<<"a is greater and b is smaller"<< endl;
+        } else {
+        cout<<"a is smaller and b is greater" << endl;
+        } }
+}
