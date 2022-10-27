8a07343b6622e05fe7baf97836a9259ea9d12e1f
Abhishek <abhishek8112003@gmail.com>
2022-10-27 23:50:53 +0530

    feat: unq in array

---
+++ FindUniqueArray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int unq(int n, int a[])
+{
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[1000] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
